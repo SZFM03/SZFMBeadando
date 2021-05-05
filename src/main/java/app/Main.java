9a434ef34e1097fdca1d583bd3ac86d3a932eaff
Boglarka Gyapjas <boglarka.gyapjas@sap.com>
2021-05-05 22:17:54 +0200

    proba5

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,7 +5,8 @@ import javafx.application.Application;
 public class Main {
     //test
     //test2
-    //test3
+    //test4
+    //test5
 
     public static void main(String[] args) {
         Application.launch(HallgatoiAdatbazisKezeloRendszer.class, args);
