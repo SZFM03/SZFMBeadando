358e554ca86398332de4011f57749041ebfd2c31
boglarkagy <bogyoka270@gmail.com>
2021-05-06 08:22:19 +0200

    teszt kommentek törlése

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,12 +3,6 @@ package app;
 import javafx.application.Application;
 
 public class Main {
-    //test
-    //test2
-    //test4
-    //test5
-    //test6
-    //test7
 
     public static void main(String[] args) {
         Application.launch(HallgatoiAdatbazisKezeloRendszer.class, args);
