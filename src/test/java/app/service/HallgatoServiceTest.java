6baee6cd0e0e80db19e3c04f463607493468c067
hepuka <79362812+hepuka@users.noreply.github.com>
2021-05-17 20:40:22 +0200

    születésidátum test

---
+++ src/test/java/app/service/HallgatoServiceTest.java
@@ -0,0 +1,36 @@
+package app.service;
+
+import app.entity.Hallgato;
+import app.entity.Tantargy;
+import app.repository.HallgatoRepository;
+import app.repository.TantargyakRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class HallgatoServiceTest {
+
+    @Mock
+    private HallgatoRepository hallgatoRepository;
+
+    @InjectMocks
+    private HallgatoService hallgatoService;
+
+    @Test
+    public void pozitivSzuletesiDatum_NegativSzuletesiSzam_HaMindenRendbenVan() {
+        // Given
+        Hallgato hallgato=new Hallgato("Hát Izsák",1977,"RR6ZUI");
+
+        // When
+        boolean szuletesidatum = hallgatoService.pozitivSzuletesiDatum(hallgato);
+
+        // Then
+        assertEquals(false, szuletesidatum);
+    }
+
+}
